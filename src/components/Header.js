763550d7ab386957b59f6bc0525f39f25a7dee4e
samro01 <srozario22@gmail.com>
2022-10-17 21:27:17 +0200

    email and password added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            
-<div className="navbar bg-primary text-primary-content">
-  <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  <Link to='/'>Home</Link>
-  <Link to='/login'>Log In</Link>
-  <Link to='/register'>Register</Link>
-</div>
+
+            <div className="navbar bg-primary text-primary-content">
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Awesome Auth</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/login'>Log In</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
+            </div>
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC3NhrS73FKdX4E8cNUarnGA-0Dhn28ouQ",
+    authDomain: "auth-router-context-498bf.firebaseapp.com",
+    projectId: "auth-router-context-498bf",
+    storageBucket: "auth-router-context-498bf.appspot.com",
+    messagingSenderId: "177431513324",
+    appId: "1:177431513324:web:df6b6aa0e9202423d03dc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
